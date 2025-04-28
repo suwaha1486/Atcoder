dbe569df344c6ee72f18fac8205e1f0b4e10bac0
SUWA Haruya <hareya.tk.1486@gmail.com>
2025-04-28 10:14:44 +0900

    AC submission

--- abc/abc403/d.py
+++ abc/abc403/d.py
@@ -1,17 +1,28 @@
-from collections import Counter
-
 n, d = map(int, input().split())
 a = list(map(int, input().split()))
 
-cnt = Counter(a)
+a_max = 10 ** 6 + 1
+cnt = [0] * a_max
+
+for i in range(n):
+    cnt[a[i]] += 1
+
 ans = 0
 
-for x in sorted(cnt):
-    if cnt[x] <= cnt[x + d]:
-        ans += cnt[x]
-        cnt[x] = 0
-    else:
-        ans += cnt[x + d]
-        cnt[x + d] = 0
+if d == 0:
+    for i in range(a_max):
+        ans += max(cnt[i] - 1, 0)
+    print(ans)
+    exit()
+
+for i in range(d):
+    x = cnt[i::d]
+    if not x:
+        continue
+    x = [0] + x
+    dp = [0] * (len(x) + 1)
+    for j in range(1, len(x)):
+        dp[j + 1] = min(dp[j] + x[j], dp[j - 1] + x[j - 1])
+    ans += dp[-1]
 
 print(ans)

8f377b8ac0ce148c6ce9a02f7aabe8c210ada748
SUWA Haruya <hareya.tk.1486@gmail.com>
2025-04-30 13:30:20 +0900

    add submission

--- abc/abc398/c.py
+++ abc/abc398/c.py
@@ -1,5 +1,3 @@
-# 解けてない
-
 n = int(input())
 a = list(map(int, input().split()))
 
@@ -14,16 +12,19 @@ if n == 2:
     else:
         print(-1)
     exit()
-    
 
 for i in range(n):
     a[i] = [a[i], i+1]
 
 a.sort(reverse=True)
+a.append((0, -1))
+a_max = 1000000001
 
-for i in range(1, n-1):
-    if a[i-1][0] != a[i][0] and a[i+1][0] != a[i][0]:
+for i in range(n):
+    if a[i][0] != a_max and a[i][0] != a[i+1][0]:
         print(a[i][1])
         exit()
+    else:
+        a_max = a[i][0]
 
 print(-1)

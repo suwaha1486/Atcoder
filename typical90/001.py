3665ffd244390f2173cd65ff995af56fdd0c292e
SUWA Haruya <hareya.tk.1486@gmail.com>
2025-05-19 22:15:05 +0900

    add new submissions

---
+++ typical90/001.py
@@ -0,0 +1,30 @@
+def check(x, a, k):
+    cnt = 0
+    tmp = 0
+    for i in range(len(a)):
+        if a[i] - tmp >= x:
+            cnt += 1
+            tmp = a[i]
+    if cnt >= k + 1:
+        return True
+    else:
+        return False
+
+def main():
+    n, l = map(int, input().split())
+    k = int(input())
+    a = list(map(int, input().split()))
+    a.append(l)
+
+    left = 1
+    right = l
+    while right - left > 1:
+        mid = (left + right) // 2
+        if check(mid, a, k):
+            left = mid
+        else:
+            right = mid
+    print(left)
+
+if __name__ == "__main__":
+    main()
---
+++ typical90/003.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def bfs(s, graph):
+    n = len(graph)
+    q = deque()
+    dist = [-1] * n
+    q.append(s)
+    dist[s] = 0
+
+    while len(q) > 0:
+        v = q.popleft()
+        for u in graph[v]:
+            if dist[u] == -1:
+                dist[u] = dist[v] + 1
+                q.append(u)
+
+    max_dist = max(dist)
+    farthest_node = dist.index(max_dist)
+    return farthest_node, max_dist
+    
+def main():
+    n = int(input())
+    graph = [[] for _ in range(n + 1)]
+
+    for _ in range(n - 1):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    farthest_node, _ = bfs(1, graph)
+    farthest_node, max_dist = bfs(farthest_node, graph)
+    
+    print(max_dist + 1)
+
+if __name__ == "__main__":
+    main()

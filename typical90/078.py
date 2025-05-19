cdd7097741c893b33f32e855a3363775a23bf193
SUWA Haruya <hareya.tk.1486@gmail.com>
2025-05-19 20:30:47 +0900

    add new submissions

---
+++ typical90/078.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[max(a, b)].append(min(a, b))
+
+cnt = 0
+for i in range(2, n + 1):
+    if len(graph[i]) == 1:
+        cnt += 1
+
+print(cnt)

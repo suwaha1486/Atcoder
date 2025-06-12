1ac335a44ac8b77dc6ea11e694d03007d19b2063
SUWA Haruya <hareya.tk.1486@gmail.com>
2025-06-12 09:56:26 +0900

    add new submission for interval length problem using two-pointer technique

---
+++ typical90/034.py
@@ -0,0 +1,26 @@
+'''
+〇〇を満たす区間の内，最小or最大の長さ
+〇〇を満たす区間を数え上げよ
+-> 尺取り法
+'''
+from collections import defaultdict
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+number = defaultdict(int)
+ans = 0
+right = 0
+
+for left in range(n):
+    # rightを可能な限り右に伸ばす
+    while right < n and (len(number) < k or a[right] in number):
+        number[a[right]] += 1
+        right += 1
+    ans = max(ans, right - left)
+    number[a[left]] -= 1
+    if number[a[left]] == 0:
+        del number[a[left]]
+
+print(ans)
+        
